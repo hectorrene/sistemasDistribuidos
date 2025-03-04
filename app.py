fc0c9e19823e334d32ca2a8cdda3e6604bf5c2ec
hectorrene <campa.hector@yahoo.com>
2025-03-03 18:45:51 -0800

    ojala

--- app.py
+++ app.py
@@ -68,4 +68,4 @@ api.add_resource(Saladas, '/saladas')
 api.add_resource(BorrarPorSabor, '/sabor/<string:nombre>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
